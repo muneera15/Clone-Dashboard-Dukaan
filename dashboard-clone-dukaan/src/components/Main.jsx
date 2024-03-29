839eac4fea437a954797215aa689fc7e83620b17
muneera <muneerashaik03@gmail.com>
2024-03-30 01:08:59 +0530

    second section of main added

--- dashboard-clone-dukaan/src/components/Main.jsx
+++ dashboard-clone-dukaan/src/components/Main.jsx
@@ -24,6 +24,128 @@ export const Main = ()=>{
     <RevenueCard3 title={"Amount Pending"} amount={"23,92,312.19"}/>
      </div>
         </section>
+        <section className='flex flex-col gap-6'>
+            <div className='flex flex-col gap-6'>
+                <h5 className='text-xl font-medium text-[#1a181e]'>Transactions | This Month</h5>
+                <div className='flex gap-3'>
+                    <button className='rounded-full text-[#808080] bg-[#e6e6e6] w-[128px] h-[36px]'>Payouts (22)</button>
+                    <button className='rounded-full text-[#ffffff] bg-[#146eb4] w-[128px] h-[36px]'>Refunds (6)</button>
+                </div>
+                <div className='flex flex-col item-center gap-3 px-3 pt-3 pb-2 rounded-[8px] bg-white'>
+                    <div className='flex justify-between'>
+                    <div className='flex items-center gap-2 pl-[15px] px-[4px] w-[300px] py-[6px] border border-[#d9d9d9] text-[#808080] rounded '>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="grey" className="w-[16.35px] h-[18px] ">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+             </svg>
+           <input type='text' className="w-[240px]"placeholder='Order ID or Transactions ID' />
+           </div>
+           <div className='flex items-center gap-3'>
+            <button  className='rounded  border p-2 border-[#d9d9d9] gap-[6px] text-[#4d4d4d] flex items-center'>Sort
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+</svg>
+</button>
+            <button className='rounded  border p-2 border-[#d9d9d9]  text-[#4d4d4d]  flex items-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+</svg>
+            </button>
+           </div>
+           </div>
+           <div>
+            <table className='min-w-full'>
+                <colgroup>
+                <col className='w-1/5'/>
+                <col className='w-1/5'/>
+                <col className='w-1/5'/>
+                <col className='w-1/5'/>
+                <col className='w-1/5'/>
+                </colgroup>
+                <thead className=' text-[#4d4d4d] bg-[#4d4d4d]'>
+                    <tr>
+                        <th className='px-3 py-[10px] text-sm font-medium tracking-wider round-l text-left'>Order ID</th>
+                        <th className='px-3 py-[10px] text-sm font-medium tracking-wider text-left'>Status</th>
+                        <th className='px-3 py-[10px] text-sm font-medium tracking-wider text-left'>Transaction ID</th>
+                        <th className='px-3 py-[10px] text-sm font-medium tracking-wider text-left'>Refund Date</th>
+                        <th className='px-3 py-[10px] text-sm font-medium tracking-wider round-r text-right'>Order Amount</th>
+                    </tr>
+                </thead>
+                <tb className="bg-white divide-[#e6e6e6]">
+                    <tr className='text-sm relative'>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr className='text-sm relative'>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    </tr>
+                    <tr className='text-sm relative'>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    </tr>
+                    <tr className='text-sm relative'>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    </tr>
+                    <tr className='text-sm relative'>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    </tr>
+                    <tr className='text-sm relative'>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    </tr>
+                    <tr className='text-sm relative'></tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <tr className='text-sm relative'>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    </tr>
+                    <tr className='text-sm relative'>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    </tr>
+                    <tr className='text-sm relative'>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    </tr>
+                </tb>
+            </table>
+           </div>
+                </div>
+            </div>
+        </section>
     </div>
 </div>
 </div>
